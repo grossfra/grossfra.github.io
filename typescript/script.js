5cf154146eb6d8e1c59c82d3c37228194bb10d5c
grossfra <franziska.gross@hs-furtwangen.de>
2019-05-24 11:07:10 +0200

    typescript

---
+++ typescript/script.js
@@ -0,0 +1,7 @@
+//hello
+console.log("Konsolen-Test");
+window.onload = function () {
+    console.log("Elemente sind bereit");
+    document.getElementById("id");
+};
+//# sourceMappingURL=script.js.map
