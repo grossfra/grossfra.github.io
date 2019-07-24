cc04e8c17d6e5f0b9de9cbacef8d9dcb267e388a
grossfra <franziska.gross@hs-furtwangen.de>
2019-07-24 21:18:40 +0200

    ts

--- Endabgabe EIA1/js/Kartenspiel.js
+++ Endabgabe EIA1/js/Kartenspiel.js
@@ -1,5 +1,33 @@
 let eigeneHandkartenArray = [];
-let gespielteKartenArray = [];
+let HandkartenGegnerArray = [];
 let KartenstapelArray = [];
-let;
+let gespielteKartenArray = [];
+function neuesDeckGenerieren() {
+    let neueKartenwertigkeit;
+    let neueKartenfarbe;
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 0; j < 4; j++) {
+            neueKartenwertigkeit = i;
+            switch (j) {
+                case 0:
+                    neueKartenfarbe = "rot";
+                    break;
+                case 1:
+                    neueKartenfarbe = "blau";
+                    break;
+                case 2:
+                    neueKartenfarbe = "gelb";
+                    break;
+                case 3:
+                    neueKartenfarbe = "grün";
+                    break;
+            }
+            let newCard = {
+                KartenWertigkeit: neueKartenwertigkeit,
+                Kartenfarbe: neueKartenfarbe
+            };
+            KartenstapelArray.push(newCard);
+        }
+    }
+}
 //# sourceMappingURL=Kartenspiel.js.map
--- Endabgabe EIA1/js/Kartenspiel.ts
+++ Endabgabe EIA1/js/Kartenspiel.ts
@@ -3,6 +3,30 @@ interface Kartenspiel {
     Kartenfarbe: string;
 }
 let eigeneHandkartenArray: Kartenspiel[] = [];
-let gespielteKartenArray: Kartenspiel[] = [];
+let HandkartenGegnerArray: Kartenspiel[] = [];
 let KartenstapelArray: Kartenspiel[] = [];
-let 
+let gespielteKartenArray: Kartenspiel[] = [];
+
+function neuesDeckGenerieren() {
+let neueKartenwertigkeit: number;
+let neueKartenfarbe: string;
+
+for(let i:number=1; i<=9; i++){
+    for(let j:number=0; j<4; j++){
+        neueKartenwertigkeit=i;
+        switch(j){
+            case 0: neueKartenfarbe = "rot"; break;
+            case 1: neueKartenfarbe = "blau"; break;
+            case 2: neueKartenfarbe = "gelb"; break;
+            case 3: neueKartenfarbe = "grün"; break;
+        }
+        let newCard: Kartenspiel = {                                         
+            KartenWertigkeit: neueKartenwertigkeit,
+            Kartenfarbe: neueKartenfarbe
+        };
+        KartenstapelArray.push(newCard);
+    }                                  
+}
+
+}
+
